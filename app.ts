ba19c0c463faa3a19ba921c2a876100a0210f3ef
Mostafa Adel <mostafadelgouda@gmail.com>
2024-10-28 10:24:07 +0300

    solving typescript problems for vercel

--- app.ts
+++ app.ts
@@ -1,20 +1,22 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const dbConnection = require("./config/database");
-const ApiError = require("./utils/apiError");
-const globalErrorHandling = require("./middlewares/errorMiddleware");
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import dbConnection from "./config/database";
+import ApiError from "./utils/apiError";
+import globalErrorHandling from "./middlewares/errorMiddleware";
 
+// Load environment variables
 dotenv.config({ path: ".env" });
-const PORT = process.env.PORT;
-const MODE = process.env.MODE;
+const PORT = process.env.PORT || 8080;
+const MODE = process.env.MODE || "production";
 
 // Database connection
+console.log(dbConnection);
 dbConnection();
 
 // Route files
-const organizationRoute = require("./routes/organizationRoute");
-const authRoute = require("./routes/authRoute");
+import organizationRoute from "./routes/organizationRoute";
+import authRoute from "./routes/authRoute";
 
 const app = express();
 
@@ -30,7 +32,7 @@ app.use("/api/v1/organizations", organizationRoute);
 app.use("/api/v1/", authRoute);
 
 // Handle undefined routes
-app.all("*", (req, res, next) => {
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
@@ -49,3 +51,5 @@ process.on("unhandledRejection", (err: Error) => {
     process.exit(0);
   });
 });
+
+export default app;
